3b1042f9e9cbe64c65cb776e23ee1c1b5c66f068
GarrixA <aphrogarrix10@gmail.com>
2025-02-27 12:13:28 +0200

    feat(initial project): project initialization

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className=" flex items-center justify-center text-3xl font-bold">
+      Welcome to apercu
+    </div>
+  );
+}
